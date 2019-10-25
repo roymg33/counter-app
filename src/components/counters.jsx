7f70f2b0bdf97ae38deb2089553b50d474d7c07e
roymg33 <tristam24@gmail.com>
2019-10-25 17:35:09 -0500

    hap

---
+++ src/components/counters.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    const {
+      onReset,
+      counters,
+      onDelete,
+      onIncrement,
+      onDecrement
+    } = this.props;
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        {counters.map(counter => (
+          <Counter
+            key={counter.id}
+            // value={counter.value}
+            // id={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
+            counter={counter}
+          ></Counter>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
